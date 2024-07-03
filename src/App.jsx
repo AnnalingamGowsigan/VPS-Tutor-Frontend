b82a67630eb429fc66a0d4a738710f975ef6c794
AnnalingamGowsigan <e18115@eng.pdn.ac.lk>
2024-07-03 15:03:29 +0530

    Add changes.

--- src/App.jsx
+++ src/App.jsx
@@ -97,6 +97,7 @@ const App = () => {
                     path: "/questionsPreview",
                     element: <PrivateRoute><QuestionsPreview /></PrivateRoute>,
                 },
+                
 
             ],
         },
